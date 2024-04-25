65c8a8bcd9eb3d54804d4bd77ff61bf27e93de87
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2024-04-25 11:47:10 +0100

    change structure

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 console.log("main process working");
 
-// import electron module
 const electron = require("electron");
 
-const app = electron.app; // Module to control application life.
-const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 const path = require("node:path");
 const url = require("node:url");
@@ -15,12 +14,11 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: false, // or true, depending on your needs
-      contextIsolation: true, // recommended (for better security)
+      nodeIntegration: false,
+      contextIsolation: true,
     },
   });
 
-  // load the index html file
   win.loadURL(
     url.format({
       pathname: path.join(__dirname, "index.html"),
@@ -29,25 +27,9 @@ function createWindow() {
     })
   );
 
-  // win.webContents.openDevTools();
-
-  // Event when the window is closed.
   win.on("closed", () => {
     win = null;
   });
 }
 
-// when electron is ready, call the createWindow function
 app.on("ready", createWindow);
-
-// app.on(`window-all-closed`, () => {
-//   if (process.platform !== `darwin`) {
-//     app.quit();
-//   }
-// });
-
-// app.on(`activate`, () => {
-//   if (win === null) {
-//     createWindow();
-//   }
-// });
